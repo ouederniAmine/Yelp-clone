ff3623378ea5c28ebd50c553684f43e693b29f76
ouederniAmine <amounaouederni@gmail.com>
2021-01-23 16:29:04 +0100

    1st

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React from "react";
+import { BusinessList } from "../BusinessList/BusinessList";
+import { SearchBar } from "../SearchBar/SearchBar";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ravenous</h1>
+      <SearchBar />
+      <BusinessList />
     </div>
   );
 }
